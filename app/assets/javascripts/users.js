cc3a0b230458d5e6885ca06fb3e851952e711ccb
Jeremy Ong <Jeremy@Jeremys-MacBook.local>
2015-08-06 18:01:40 +0800

    do sign up views ajax compatible

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,110 @@
+$(function() {
+
+	$("footer").css("display", "none");
+
+	$("#slider").slider({
+			range: "min",
+			value: 500,
+			min: 0,
+			max: 1000,
+			step: 10,
+			slide: function(event, ui) {
+				$("#amount-label").val("MYR " + ui.value);
+			}
+	});
+
+	$("#amount").val($("#slider").slider("value"));
+	$("#amount-label").val("MYR " + $("#slider").slider("value"));
+
+	var packagesArray = [];
+	var emailFormat = /^[^@\s]+@[^@\s.]+\.[\w]+/i;
+
+	$(".activity-option").click(function(){
+		$(this).toggleClass("activity-option-selected")
+		$(this).find(".fa").toggleClass("fa-selected")
+		$(this).find(".fa-circle").toggleClass("hidden")
+		$(this).find(".fa-check-circle").toggleClass("hidden")
+		var packageText = $(this).find(".activity-text").text()
+		if ($.inArray(packageText, packagesArray) == -1) {
+			packagesArray.push(packageText);
+		}
+		else {
+			var index = packagesArray.indexOf(packageText);
+			packagesArray.splice(index ,1)
+		}
+	});
+
+	$("#first-button").click(function(e){
+		if (packagesArray == "") {
+			e.preventDefault();
+			$(".activity-error").css("display","inherit");
+			return false;
+		}
+		else
+			return true;
+	});
+
+	$("#second-button").click(function(e){
+		if ($("#amount").val() == 0) {
+				e.preventDefault();
+				$(".budget-error").css("display","inherit");
+				return false;}
+		else {
+				return true;}
+	});
+
+ 	function detailsEmpty(){
+		$(".error-message" ).css("display","none")
+		$(".details-error" ).css("display","none")
+		$(".email-error" ).css("display","none")
+		$(".sign-up-success").css("display","none")
+
+		var userName = $("#user-name").val();
+		var userEmail = $("#user-email").val();
+
+ 		if (userName == "" || userEmail == "") {
+			$(".error-message").css("display","block");
+			$(".details-error").css("display","block");
+			return false;
+		}
+		else if (emailFormat.test(userEmail) === false) {
+			$(".error-message").css("display","block");
+			$(".email-error").css("display","block");
+			return false;
+		}
+		else {
+			$(".error-message" ).css("display","none");
+			$(".details-error" ).css("display","none");
+			$(".email-error" ).css("display","none");
+			$(".jumbotron").hide();
+			$(".loading-gif").appendTo("body");
+			$(".loading-gif").css("display","block")
+			return true;
+		}
+ 	}
+
+	$("#btn-submit").click(function(e){
+		detailsEmpty();
+		e.preventDefault();
+		var maxBudget = $("#amount").val()
+		var feedback = $("#feedback").val()
+		var userName = $("#user-name").val()
+		var userEmail = $("#user-email").val()
+		
+		$.ajax({
+			type: "POST",
+			url: '/users',
+			data: {
+				packages_selected: packagesArray,
+				max_budget: maxBudget,
+				feedback: feedback,
+				user_name: userName,
+				user_email: userEmail}
+			}).done(function(result) {
+				debugger
+				$(".loading-gif").css("display","none");
+	  			$("#carousel").html(result);
+			});
+
+		});
+});
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Flavor.create(name: "Menthol & Mint")
+Flavor.create(name: "Fruity")
+Flavor.create(name: "Creamy")
+Flavor.create(name: "Candy")
+Flavor.create(name: "Tobacco")
+Flavor.create(name: "Cloudy (Awan)")
+Flavor.create(name: "Drinks")
+Flavor.create(name: "Coffee")
