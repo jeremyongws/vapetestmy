3ef92ac336587aab0d19cd97aa6d8278ecd89a31
Jeremy Ong <Jeremy@Jeremys-MacBook.local>
2015-08-07 16:33:47 +0800

    fix issue

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
+  post 'memberships/new'
   # get 'memberships/new' => 'memberships#new'
 
   # Example of regular route:
